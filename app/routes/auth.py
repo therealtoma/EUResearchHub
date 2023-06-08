accb5d9d23c90834d804344f9dc4da4b5b72b5c5
andreramolivaz <92636448+andreramolivaz@users.noreply.github.com>
2023-06-08 19:20:02 +0200

    Implementato basic protezione xss

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -8,6 +8,9 @@ from flask_login import login_user, logout_user, login_required, current_user
 from app.utils.utils import check_email
 from shutil import copy
 import os
+from flask import render_template, request, redirect, url_for, flash, Markup
+from markupsafe import escape
+from bleach import clean
 
 auth = Blueprint('auth', __name__)
 
@@ -18,19 +21,20 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email') # email from the form
         password = request.form.get('password') # password from the form
-
+        sanitized_email = clean(email)
+        sanitized_password = clean(password)
         usertype = request.form.get('usertype') # type of user that whants to login
 
-        evaluator = Evaluators.query.filter_by(email=email).first()
-        researcher = Researchers.query.filter_by(email=email).first()
+        evaluator = Evaluators.query.filter_by(email=sanitized_email).first()
+        researcher = Researchers.query.filter_by(email=sanitized_email).first()
 
-        if evaluator and check_password_hash(evaluator.password, password):
+        if evaluator and check_password_hash(evaluator.password, sanitized_password):
             login_user(evaluator)
             db.session.close()
             current_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('EVALUATOR_DATABASE_URI')
             session['user_type'] = 'evaluator'
             return redirect(url_for('views.projects'))
-        elif researcher and check_password_hash(researcher.password, password):
+        elif researcher and check_password_hash(researcher.password, sanitized_password):
             login_user(researcher)
             db.session.close()
             current_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('RESEARCHER_DATABASE_URI')
@@ -57,19 +61,25 @@ def register():
         email = request.form.get('email') # email from the form
         password = request.form.get('password') # password from the form
         affiliation = request.form.get('affiliation') # affiliation from the form
+        sanitized_name = clean(name)
+        sanitized_surname = clean(surname)
+        sanitized_email = clean(email)
+        sanitized_password = clean(password)
+
+
         
         # if the affiliation is not set, the user is an evaluator, otherwise a researcher
         # also check if email is already registerd on other table, error in case
         if affiliation is None:
-            user = Evaluators.query.filter_by(email=email).first()
-            user_other = Researchers.query.filter_by(email=email).first()
+            user = Evaluators.query.filter_by(email=sanitized_email).first()
+            user_other = Researchers.query.filter_by(email=sanitized_email).first()
         else:
-            user = Researchers.query.filter_by(email=email).first()
-            user_other = Evaluators.query.filter_by(email=email).first()
+            user = Researchers.query.filter_by(email=sanitized_email).first()
+            user_other = Evaluators.query.filter_by(email=sanitized_email).first()
             
         if user or user_other:
             flash('Email already registered', category='error')
-        elif not check_email(email):
+        elif not check_email(sanitized_email):
             flash('Email format not valid', category='error')
         elif len(password) < 7:
             flash('Password must be 8 characters', category='error')
@@ -80,17 +90,17 @@ def register():
                 current_directory = os.path.dirname(os.path.realpath(__file__))
                 current_app.config['UPLOAD_FOLDER'] = os.path.join(current_directory,
                                                                    '../static/uploads/profile_images')
-                filename = f'{email}.jpg'
+                filename = f'{sanitized_email}.jpg'
                 profile_picture.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
             else:
                 filename = 'default.jpg'
 
             if affiliation is None:
-                new_user = Evaluators(name=name, surname=surname, email=email,
-                                      password=generate_password_hash(password, method='sha256'), profile_picture=filename)
+                new_user = Evaluators(name=sanitized_name, surname=sanitized_surname, email=sanitized_email,
+                                      password=generate_password_hash(sanitized_password, method='sha256'), profile_picture=filename)
             else:
-                new_user = Researchers(name=name, surname=surname, email=email,
-                                       password=generate_password_hash(password, method='sha256'),
+                new_user = Researchers(name=sanitized_name, surname=sanitized_surname, email=sanitized_email,
+                                       password=generate_password_hash(sanitized_password, method='sha256'),
                                        affiliation=affiliation, profile_picture=filename)
 
             db.session.add(new_user)
